dc2582d14b4824bc3f8d2cbd8614b6293bca44f8
Akhilpalthi <akhilpalthi@gmail.com>
2024-04-13 23:59:02 +0530

    Sample

---
+++ Lab002.java
@@ -0,0 +1,17 @@
+package ex_11042024;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        System.out.print("Hello Pramod");
+        System.out.print("Hello Dutta");
+        System.out.print("Hello Pramod\t Dutta");
+        System.out.print("Hello Pramod\n Dutta");
+        System.out.println("Hello Pramod\n Dutta");
+        System.out.println("Hello Pramod\n Dutta");
+
+        //println ->print in new line
+        //\n new line. , \t tab
+
+
+    }
+}
